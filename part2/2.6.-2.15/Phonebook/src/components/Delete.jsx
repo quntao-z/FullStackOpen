5e544467dc95acd500166f388eac0f7f97cf5dc0
Quntao Zheng <quntaozheng@gmail.com>
2023-12-13 23:26:55 -0500

    changed PhoneBook to be top directory

--- part2/2.6.-2.15/Phonebook/src/components/Delete.jsx
+++
@@ -1,9 +0,0 @@
-const Delete = ({ deletePerson, personId }) => {
-  return (
-    <>
-      <button onClick={(e) => deletePerson(e, personId)}> Delete </button>
-    </>
-  );
-};
-
-export default Delete;
